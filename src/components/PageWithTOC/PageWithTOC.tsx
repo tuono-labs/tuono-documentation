cc261bf2c37575852083041524f9fc71295ed5a1
Marco Pasqualetti <marco.pasqualetti@live.com>
2025-03-03 13:47:22 +0100

    refactor: enable `react/jsx-no-useless-fragment`

--- src/components/PageWithTOC/PageWithTOC.tsx
+++ src/components/PageWithTOC/PageWithTOC.tsx
@@ -1,5 +1,5 @@
 import type { JSX, ReactNode } from 'react'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Box, Container } from '@mantine/core'
 import { useViewportSize } from '@mantine/hooks'
 
@@ -14,7 +14,6 @@ interface PageWithTOCProps {
 export function PageWithTOC({ children }: PageWithTOCProps): JSX.Element {
   const { width } = useViewportSize()
   const [footerHeight, setFooterHeight] = useState<number>(0)
-  const mainRef = useRef<HTMLDivElement>(null)
   const height = `calc(100% - ${footerHeight}px)`
 
   useEffect(() => {
@@ -24,39 +23,35 @@ export function PageWithTOC({ children }: PageWithTOCProps): JSX.Element {
       setFooterHeight(footer.clientHeight)
     }
   }, [
-    setFooterHeight,
     // Reload on window width resize
     width,
   ])
 
   return (
-    <>
-      <Box
-        style={{ zIndex: 10, height }}
-        pos="relative"
-        ref={mainRef}
-        mb={footerHeight}
-        bg="var(--mantine-color-body)"
+    <Box
+      style={{ zIndex: 10, height }}
+      pos="relative"
+      mb={footerHeight}
+      bg="var(--mantine-color-body)"
+    >
+      <Container
+        size={1000}
+        w="100%"
+        display="flex"
+        style={{ gap: 12, justifyContent: 'space-between' }}
       >
-        <Container
-          size={1000}
-          w="100%"
-          display="flex"
-          style={{ gap: 12, justifyContent: 'space-between' }}
+        <Box
+          id="mdx-root"
+          component="article"
+          mt="xl"
+          py={36}
+          className={classes.wrapper}
         >
-          <Box
-            id="mdx-root"
-            component="article"
-            mt="xl"
-            py={36}
-            className={classes.wrapper}
-          >
-            {children}
-          </Box>
+          {children}
+        </Box>
 
-          <TableOfContents />
-        </Container>
-      </Box>
-    </>
+        <TableOfContents />
+      </Container>
+    </Box>
   )
 }
