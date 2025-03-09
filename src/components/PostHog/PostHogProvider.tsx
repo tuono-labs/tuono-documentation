8eb1cfcd3c472753f194653d59ae0a6dea42745d
Marco Pasqualetti <marco.pasqualetti@live.com>
2025-03-09 13:45:32 +0100

    chore: update to tuono v0.17.10

--- src/components/PostHog/PostHogProvider.tsx
+++ src/components/PostHog/PostHogProvider.tsx
@@ -11,13 +11,13 @@ export default function PostHogProvider({
   children,
 }: PostHogProviderProps): JSX.Element {
   useEffect(() => {
-    if (import.meta.env.VITE_ENABLE_POSTHOG === 'true') {
-      posthogJs.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
-        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+    if (import.meta.env.TUONO_PUBLIC_POSTHOG_ENABLE === 'true') {
+      posthogJs.init(import.meta.env.TUONO_PUBLIC_POSTHOG_KEY, {
+        api_host: import.meta.env.TUONO_PUBLIC_POSTHOG_HOST,
         persistence: 'memory', // Cookieless tracking
         disable_persistence: true, // Disable persistence
         loaded: (ph) => {
-          if (import.meta.env.VITE_ENV === 'development') ph.debug()
+          if (import.meta.env.DEV) ph.debug()
         },
       })
     }
