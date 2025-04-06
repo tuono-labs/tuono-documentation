8b724ec39b4b272df28a8e74e45aa1cb18a4c83c
Valerio Ageno <valerioageno@yahoo.it>
2025-04-06 17:32:59 +0200

    feat: create typography classes

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,3 @@
+export default function Hero() {
+  return <h1 className="main-heading">Modern fullstack framework</h1>
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,6 @@
+import type { JSX } from 'react'
+import Hero from '@/components/Hero'
+
+export default function IndexPage(): JSX.Element {
+  return <Hero />
+}
