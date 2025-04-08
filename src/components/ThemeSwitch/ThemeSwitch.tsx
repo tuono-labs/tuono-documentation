a146e0fb8191f3deac063e07ed7cf3b91c6309af
Camille Maisonobe <48532925+CMaiso@users.noreply.github.com>
2025-04-08 18:58:42 +0200

    feat: create theme switcher logic (#85)

---
+++ src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect, type JSX } from 'react'
+
+import { useTheme } from 'next-themes'
+
+import styles from './theme-switch.module.scss'
+
+export default function ThemeSwitch(): JSX.Element {
+  const [mounted, setMounted] = useState(false)
+  const { resolvedTheme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  // useTheme will be undefined until mounted on the client. This means if we
+  // try to render UI based on the current theme before mounting on the client,
+  // we will get a hydration mismatch error.
+  // docs: https://www.npmjs.com/package/next-themes
+  if (!mounted) {
+    return <div className="theme-switch-container" />
+  }
+
+  return (
+    <button
+      className={styles['theme-switch-button']}
+      onClick={() => {
+        setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+      }}
+    >
+      <span className="caption bold">
+        {resolvedTheme === 'dark' ? `Light` : `Dark`}
+      </span>
+    </button>
+  )
+}
