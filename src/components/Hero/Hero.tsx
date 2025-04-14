45fa048e5110a100e525f73ffe964efaa95b4ae0
Valerio Ageno <valerioageno@yahoo.it>
2025-04-14 16:39:16 +0200

    fix: linting

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,11 @@
 import type { JSX } from 'react'
+
 import { Chip } from '../Chip'
 
 export function Hero(): JSX.Element {
   return (
     <>
-      <Chip onPress={() => console.log('ciao')}>just published v0.19.4</Chip>
+      <Chip>just published v0.19.4</Chip>
       <h1 className="main-heading">Modern fullstack framework</h1>
     </>
   )
