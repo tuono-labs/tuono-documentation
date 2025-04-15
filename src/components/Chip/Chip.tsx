30e5c220312a4d26e5bb1d13e44cb2a55710ef16
Valerio Ageno <valerioageno@yahoo.it>
2025-04-15 10:21:20 +0200

    return JSX element

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react'
+import type { JSX, ReactNode } from 'react'
 import type { ButtonProps } from 'react-aria-components'
 import { Button } from 'react-aria-components'
 import cx from 'clsx'
@@ -9,7 +9,7 @@ interface ChipProps extends ButtonProps {
   children: ReactNode
 }
 
-export function Chip({ children, ...rest }: ChipProps): ReactNode {
+export function Chip({ children, ...rest }: ChipProps): JSX.Element {
   const hasOnPressHandler = typeof rest.onPress !== 'undefined'
   return (
     <Button
